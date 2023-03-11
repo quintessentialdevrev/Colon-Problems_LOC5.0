f3f264691ed45c33015f24241dec050725874ac2
quintessentialdevrev <63969941+quintessentialdevrev@users.noreply.github.com>
2023-03-11 13:14:07 +0530

    merge satvam

--- colon-problems/src/App.jsx
+++ colon-problems/src/App.jsx
@@ -5,6 +5,8 @@ import {ChakraProvider} from '@chakra-ui/react'
 import Login from './components/Authentication/Login'
 import Signup from './components/Authentication/Signup'
 import Loading from './components/Loading/Loading';
+import Home from './components/Home/Home';
+
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/loading" element={<Loading/>}/>
+          <Route path="/home" element={<Home/>}/>
         </Routes>
       </Router>
             
---
+++ colon-problems/src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContactUs from '../contactus'
+import Statistics from '../statistics'
+import Testimonial from '../testimonial'
+
+export default function() {
+  return (
+    <div>
+    <Testimonial />
+    <Statistics />
+    <ContactUs />
+    </div>
+  )
+}
