97777a51edcd3f7f1183ba2b2ca30cf0340af544
Yash Thakar <90375840+yashyaks@users.noreply.github.com>
2023-03-12 00:42:48 +0530

    stockimgs changes

--- colon-problems/src/components/stock_imgs/main_container.jsx
+++ colon-problems/src/components/stock_imgs/main_container.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Data from "./image_data";
 import './maincontainer.css';
 import Pin from './Pin';
+import { Search2Icon } from '@chakra-ui/icons';
 
 export default function Main_container() {
   return (
@@ -11,10 +12,7 @@ export default function Main_container() {
         <div className="searchBox">
           <input type="text" placeholder="Search" />
           <div className="search">
-            {/* <img
-              src="https://firebasestorage.googleapis.com/v0/b/codewithvetriapi-c56e3.appspot.com/o/icons8-forward-arrow-100.png?alt=media&token=3f56e775-43c1-41d3-a0c4-90217b31b5be"
-              alt=""
-            /> */}
+            <Search2Icon/>
           </div>
         </div>
         <div className="mainContainer">
