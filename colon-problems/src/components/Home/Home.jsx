1c890544adaab312829d9f622dee953bfe991dd6
HiyaJain22 <105354219+HiyaJain22@users.noreply.github.com>
2023-03-11 14:49:57 +0530

    chill satvam

--- colon-problems/src/components/Home/Home.jsx
+++ colon-problems/src/components/Home/Home.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import ContactUs from '../contactus'
 import Statistics from '../statistics'
 import Testimonial from '../testimonial'
+import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer'
 
 export default function() {
   return (
     <div>
+    <Navbar/>
     <Testimonial />
     <Statistics />
     <ContactUs />
+    <Footer/>
+  
     </div>
   )
 }
