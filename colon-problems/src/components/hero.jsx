919eddd5cdfea146e86056969637fcbd5c115950
HiyaJain22 <105354219+HiyaJain22@users.noreply.github.com>
2023-03-12 03:12:27 +0530

    hiya3

--- colon-problems/src/components/hero.jsx
+++ colon-problems/src/components/hero.jsx
@@ -1,62 +1,60 @@
 import {
-    Button,
-    Flex,
-    Heading,
-    Image,
-    Stack,
-    Text,
-    useBreakpointValue,
-  } from '@chakra-ui/react';
-  import Spline from '@splinetool/react-spline';
-  
-  export default function Hero() {
-    return (
-      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-        <Flex p={8} flex={1} align={'center'} justify={'center'}>
-          <Stack spacing={6} w={'full'} maxW={'lg'}>
-            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-              <Text
-                as={'span'}
-                position={'relative'}
-                _after={{
-                  content: "''",
-                  width: 'full',
-                  height: useBreakpointValue({ base: '20%', md: '30%' }),
-                  position: 'absolute',
-                  bottom: 1,
-                  left: 0,
-                  bg: 'blue.400',
-                  zIndex: -1,
-                }}>
-                Freelance
-              </Text>
-              <br />{' '}
-              <Text color={'blue.400'} as={'span'}>
-                Design Projects
-              </Text>{' '}
-            </Heading>
-            <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-              The project board is an exclusive resource for contract work. It's
-              perfect for freelancers, agencies, and moonlighters.
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import Spline from '@splinetool/react-spline';
+
+export default function Hero() {
+  return (
+    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+      <Flex p={8} flex={1} align={'center'} justify={'center'}>
+        <Stack spacing={6} w={'full'} maxW={'lg'}>
+          <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+            <Text
+              as={'span'}
+              position={'relative'}
+              _after={{
+                content: "''",
+                width: 'full',
+                height: useBreakpointValue({ base: '20%', md: '30%' }),
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                bg: 'purple.400',
+                zIndex: -1,
+              }}>
+              WELCOME
             </Text>
-            <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
-              <Button
-                rounded={'full'}
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Create Project
-              </Button>
-              <Button rounded={'full'}>How It Works</Button>
-            </Stack>
+            <br />{' '}
+            <Text color={'purple.400'} as={'span'}>
+              TO PIKONE
+            </Text>{' '}
+          </Heading>
+          <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum debitis suscipit minima reprehenderit laboriosam, molestias nulla dignissimos? Sapiente non ipsum exercitationem itaque quidem voluptatem, iure ullam nisi asperiores hic officiis!
+          </Text>
+          <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+            <Button
+              rounded={'full'}
+              bg={'purple.400'}
+              color={'white'}
+              _hover={{
+                bg: 'purple.500',
+              }}>
+              Search
+            </Button>
+            <Button rounded={'full'}>Explore Events</Button>
           </Stack>
-        </Flex>
-        <Flex flex={1}>
-            <Spline scene="https://prod.spline.design/ENWapOdF16uKVuSe/scene.splinecode" />
-        </Flex>
-      </Stack>
-    );
-  }
-  
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Spline scene="https://prod.spline.design/ENWapOdF16uKVuSe/scene.splinecode" />
+      </Flex>
+    </Stack>
+  );
+}
